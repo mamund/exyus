27bd529b0117bb9110cfddd6c8e4ce71011361e2
mca@amundsen.com <mca@amundsen.com@921b4b22-612f-0410-885e-230a397dab82>
2008-01-12 20:50:17 +0000

    added Postable.cs to show example of x-www-form-encoded support altered StaticExamples.cs to include link to postable url

---
+++ trunk/Exyus.Samples/Postable.cs
@@ -0,0 +1,37 @@
+using System;
+using Exyus.Web;
+
+namespace Exyus.Samples.Postable
+{
+    class Postable
+    {
+        // simple direct get handler
+        [UriPattern(@"/postable/(?<docid>[^/?]*)?(?:\.xcs)(?:[?])?(?:.*)?")]
+        [MediaTypes("text/html")]
+        public class postPages : XmlFileResource
+        {
+            public postPages()
+            {
+                this.ContentType = "text/html";
+                this.UpdateMediaTypes = new string[] { "text/html" };
+                this.AllowPost = true;
+                this.RedirectOnPost = true;
+                this.AllowCreateOnPut = false;
+                this.PostLocationUri = "/postable/";
+                this.DocumentsFolder = "~/documents/postable/";
+                this.StorageFolder = "~/storage/postable/";
+                this.XHtmlNodes = new string[] { "//body" };
+                this.LocalMaxAge = 600;
+                this.UpdateMediaTypes = new string[]
+                {
+                    "application/x-www-form-urlencoded"
+                };
+                this.ImmediateCacheUriTemplates = new string[]
+                {
+                    "/postable/.xcs",
+                    "/postable/{docid}.xcs"
+                };
+            }
+        }
+    }
+}
