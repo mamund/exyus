ce2add9c1f279ed4adb05b44e1e6c4b810acc043
mca@amundsen.com <mca@amundsen.com@921b4b22-612f-0410-885e-230a397dab82>
2008-01-29 11:05:50 +0000

    tasklist-cmd   * added ewn commandline app to show off HTTPClient

---
+++ trunk/tasklist-cmd/tasklist-cmd/bin/Debug/set-tasks.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+tasklist-cmd -clear
+tasklist-cmd -add "read tutorials" 1
+tasklist-cmd -add "download exyus" 
+tasklist-cmd -add "check out newsgroup" 1
+tasklist-cmd -add "start writing my own cool apps!"
+
