596146b5f3a375ab660d667ecbb08f55ccdeb533
mca@amundsen.com <mca@amundsen.com@921b4b22-612f-0410-885e-230a397dab82>
2008-03-16 02:30:51 +0000

    Program.cs  ** added new demo console app to talk to UGData URI

---
+++ ugdata-cmd/ugdata-cmd/bin/Debug/ugscript.cmd
@@ -0,0 +1,11 @@
+﻿@echo off
+rem scripted update for ugdata
+rem 2008-03-14 (mca)
+
+ugdata-cmd -clear
+ugdata-cmd -add Mike Amundsen 1999-01-01 GoogleRocks
+ugdata-cmd -add Mary Michelson 1989-01-01 CallMeRoy
+ugdata-cmd -add Marvin Marrison 1979-01-01 None
+ugdata-cmd -add Mandy Marvinson 1969-01-01 ICanHitF1
+ugdata-cmd -add Mark Mandyville 1959-01-01 WhatIsREST
+
