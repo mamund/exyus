7c1370f61b6159a8ef6655df6561deb8ff99110b
mca@amundsen.com <mca@amundsen.com@921b4b22-612f-0410-885e-230a397dab82>
2008-04-13 02:25:35 +0000

    CodeBreaker.cs   * modifed URI mappings   * added support for Feedback page (sends email)

--- Exyus.Samples/CodeBreaker.cs
+++ Exyus.Samples/CodeBreaker.cs
@@ -27,6 +27,7 @@ namespace Exyus.Samples.CodeBreak
 
     public override void Get()
     {
+      // create the codebreaker-id cookie if none exists
       string uid = util.CookieRead(CBData.CodeKeyID);
       if (uid == string.Empty)
       {
@@ -53,6 +54,7 @@ namespace Exyus.Samples.CodeBreak
       this.AllowPost = false;
     }
 
+    // re-route based on cookie discovery
     public override void Get()
     {
       string uid = util.CookieRead(CBData.CodeKeyID);
@@ -89,11 +91,11 @@ namespace Exyus.Samples.CodeBreak
       this.UpdateMediaTypes = new string[] { "application/x-www-form-urlencoded" };
 
       this.ImmediateCacheUriTemplates = new string[]
-                {
-                    "/codebreaker/.xcs",
-                    "/codebreaker/{uid}/games/.xcs",
-                    "/codebreaker/{uid}/games/{id}.xcs",
-                };
+        {
+            "/codebreaker/.xcs",
+            "/codebreaker/{uid}/games/.xcs",
+            "/codebreaker/{uid}/games/{id}.xcs",
+        };
     }
 
     // return either this user's games or the current game
@@ -143,11 +145,11 @@ namespace Exyus.Samples.CodeBreak
   // handle moves (plays) in the game - note the <gid>
   [UriPattern(@"/codebreaker/(?<uid>.*)/games/(?<gid>[0-9]*)/moves/(?<id>[0-9]*)?\.xcs")]
   [MediaTypes("text/html")]
-  class MoveResource : XmlSqlResource
+  class Moves : XmlSqlResource
   {
     Utility util = new Utility();
 
-    public MoveResource()
+    public Moves()
     {
       this.AllowCreateOnPut = false;
       this.AllowDelete = false;
@@ -161,13 +163,15 @@ namespace Exyus.Samples.CodeBreak
       this.UpdateMediaTypes = new string[] { "application/x-www-form-urlencoded" };
 
       this.ImmediateCacheUriTemplates = new string[]
-                {
-                    "/codebreaker/.xcs",
-                    "/codebreaker/{uid}/games/.xcs",
-                    "/codebreaker/{uid}/games/{gid}.xcs",
-                    "/codebreaker/{uid}/games/{gid}/moves/.xcs",
-                    "/codebreaker/{uid}/games/{gid}/moves/{id}.xcs"
-                };
+          {
+              "/codebreaker/.xcs",
+              "/codebreaker/{uid}/games/.xcs",
+              "/codebreaker/{uid}/games/{gid}.xcs",
+              "/codebreaker/{uid}/games/{gid}/moves/.xcs",
+              "/codebreaker/{uid}/games/{gid}/moves/{id}.xcs",
+              "/codebreaker/players/.xcs",
+              "/codebreaker/players/{uid}.xcs"
+          };
     }
 
     // get list of moves for this game or show a single move
@@ -203,24 +207,206 @@ namespace Exyus.Samples.CodeBreak
     }
   }
 
+  // handle display of players
+  // noe4 that no cookie is needed here
+  [UriPattern(@"/codebreaker/players/(?<id>[\w-]*)\.xcs")]
+  [MediaTypes("text/html")]
+  class Players : XmlSqlResource
+  {
+    public Players()
+    {
+      this.AllowCreateOnPut = false;
+      this.AllowDelete = false;
+      this.AllowPut = false;
+      this.AllowPost = false;
+      this.ContentType = "text/html";
+      this.DocumentsFolder = "~/documents/codebreaker/players/";
+      this.ConnectionString = "exyus_samples";
+      this.UseValidationCaching = true;
+      this.LocalMaxAge = 600;
+
+      this.ImmediateCacheUriTemplates = new string[]
+      {
+          "/codebreaker/players/.xcs",
+          "/codebreaker/players/{id}.xcs"
+      };
+    }
+  }
+
+  // handle redirect for proper POSTing of player name
+  [UriPattern(@"/codebreaker/name/.xcs")]
+  [MediaTypes("text/html")]
+  class NameRoot : HTTPResource
+  {
+    Utility util = new Utility();
+
+    public NameRoot()
+    {
+      this.ContentType = "text/html";
+      this.AllowDelete = false;
+      this.AllowPost = false;
+    }
+
+    // use as a redirect based on cookie
+    public override void Get()
+    {
+      string uid = util.CookieRead(CBData.CodeKeyID);
+      if (uid == string.Empty)
+      {
+        this.Context.Response.Redirect(util.GetConfigSectionItem(Constants.cfg_exyusSettings, Constants.cfg_rootfolder) + "/codebreaker/");
+      }
+      else
+      {
+        this.Context.Response.Redirect(util.GetConfigSectionItem(Constants.cfg_exyusSettings, Constants.cfg_rootfolder) + string.Format("/codebreaker/{0}/name/", uid));
+      }
+    }
+  }
+
+  // handle POSTing of name for a user
+  // requires the cookie is set properly
+  [UriPattern(@"/codebreaker/(?<uid>.*)/name/(?<id>[\w]*)\.xcs")]
+  [MediaTypes("text/html")]
+  class Names : XmlSqlResource
+  {
+    Utility util = new Utility();
+
+    public Names()
+    {
+      this.AllowCreateOnPut = false;
+      this.AllowDelete = false;
+      this.RedirectOnPost = true;
+      this.ContentType = "text/html";
+      this.DocumentsFolder = "~/documents/codebreaker/name/";
+      this.ConnectionString = "exyus_samples";
+      this.UseValidationCaching = true;
+      this.LocalMaxAge = 600;
+      this.PostLocationUri = "/codebreaker/players/{id}";
+      this.UpdateMediaTypes = new string[] { "application/x-www-form-urlencoded" };
+
+      this.ImmediateCacheUriTemplates = new string[]
+      {
+          "/codebreaker/{uid}/name/.xcs",
+          "/codebreaker/{uid}/name/{id}.xcs",
+          "/codebreaker/players/.xcs",
+          "/codebreaker/players/{id}.xcs",
+          "/codebreaker/{uid}/games/.xcs"
+      };
+    }
+
+    // handle form if *no* id
+    public override void Get()
+    {
+      // no cookie, go to start again
+      string uid = util.CookieRead(CBData.CodeKeyID);
+      string id = (ArgumentList["id"]!=null?ArgumentList["id"].ToString():string.Empty);
+
+      // no cookie or existing doc id
+      if (uid.Length == 0 || id.Length!=0)
+      {
+        this.Context.Response.Redirect(util.GetConfigSectionItem(Constants.cfg_exyusSettings, Constants.cfg_rootfolder) + string.Format("/codebreaker/players/{0}",id));
+      }
+
+      // if cookie is not the same, go to the proper url
+      if (uid != ArgumentList["uid"].ToString())
+      {
+        this.Context.Response.Redirect(util.GetConfigSectionItem(Constants.cfg_exyusSettings, Constants.cfg_rootfolder) + string.Format("/codebreaker/{0}/names/", id));
+      }
+
+      base.Get();
+    }
+
+    // create a new name for this user
+    public override void Post()
+    {
+      // no cookie or wrong cookie, throw error
+      string uid = util.CookieRead(CBData.CodeKeyID);
+      if (uid.Length == 0 || uid != ArgumentList["uid"].ToString())
+      {
+        throw new HttpException(403, "access denied");
+      }
+
+      base.Post();
+    }
+  }
+
   // about page
   [UriPattern(@"/codebreaker/about/\.xcs")]
   [MediaTypes("text/html")]
-  class CodeBreakerAbout : XmlPageResource
+  class About : XmlPageResource
   {
-    public CodeBreakerAbout()
+    public About()
     {
       this.ContentType = "text/html";
       this.LocalMaxAge = 600;
       this.TemplateXsl = "~/documents/codebreaker/about/about_{ftype}.xsl";
     }
   }
+
+  // feedback form page
+  [UriPattern(@"/codebreaker/feedback/\.xcs")]
+  [MediaTypes("text/html")]
+  class Feedback : XmlPageResource
+  {
+    public Feedback()
+    {
+      this.ContentType = "text/html";
+      this.LocalMaxAge = 600;
+      this.TemplateXsl = "~/documents/codebreaker/feedback/form_{ftype}.xsl";
+    }
+  }
+
+  // feedback (sendmail) page
+  [UriPattern(@"/codebreaker/feedback/post\.xcs")]
+  [MediaTypes("text/html")]
+  class FeedbackPost : SMTPResource
+  {
+    public FeedbackPost()
+    {
+      this.AllowPost = true;
+      this.AllowGet = true;
+      this.ContentType = "text/xml";
+      this.DocumentsFolder = "~/documents/codebreaker/feedback/";
+      this.PostLocationUri = "/codebreaker/feedback/thankyou";
+      this.RedirectOnPost = true;
+      this.XHtmlNodes = new string[] { "//body" };
+      this.UpdateMediaTypes = new string[]
+      {
+        "application/x-www-form-urlencoded"
+      };
+    }
+  }
+
+  // feedback thankyou page
+  [UriPattern(@"/codebreaker/feedback/thankyou\.xcs")]
+  [MediaTypes("text/html")]
+  class FeedbackThankyou :  XmlPageResource
+  {
+    public FeedbackThankyou()
+    {
+      this.ContentType = "text/html";
+      this.LocalMaxAge = 600;
+      this.TemplateXsl = "~/documents/codebreaker/feedback/thankyou_{ftype}.xsl";
+    }
+  }
+
+  // deliver css file
+  [UriPattern(@"/codebreaker/css/main\.xcs")]
+  [MediaTypes("text/css")]
+  class CSSMain : StaticResource
+  {
+    public CSSMain()
+    {
+      this.ContentType = "text/css";
+      this.Content = Helper.ReadFile("~/documents/codebreaker/css/main.css");
+    }
+  }
+
   // source code viewer
   [UriPattern(@"/codebreaker/source/\.xcs")]
   [MediaTypes("text/html")]
-  class CodeBreakerSource : PlainTextViewer
+  class Source : PlainTextViewer
   {
-    public CodeBreakerSource()
+    public Source()
     {
       this.MaxAge = 600;
       this.UseValidationCaching = true;
@@ -246,7 +432,6 @@ namespace Exyus.Samples.CodeBreak
   class CBData
   {
     public static string CodeKeyID = "codebreaker-id";
-    public static string cacheBot = "exyus-cache-bot";
   }
 
   // generate a new answer set
